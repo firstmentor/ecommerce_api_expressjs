5e15952dbb2eb83be200689d37e2b3a5ca3e362d
firstmentor <jainvikas887@gmail.com>
2023-12-01 15:13:34 +0530

    heloq

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
  
 const connectDB = ()=>{
     // return mongoose.connect(process.env.DB_URL) 
-    return mongoose.connect(process.env.DB_URL_LOCAL) 
+    return mongoose.connect(process.env.DB_URL) 
     .then((data)=>{
         console.log(`Mongodb connected with server:${data.connection.host}`);
     })
