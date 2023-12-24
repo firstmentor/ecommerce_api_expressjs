c4ff1d276fcf58d82b039862e7ed6389b4e29d0e
firstmentor <jainvikas887@gmail.com>
2023-12-24 11:24:35 +0530

    update

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,34 +3,34 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 var cloudinary = require('cloudinary').v2;
 
-cloudinary.config({ 
-    cloud_name: 'depjzfj9a', 
-    api_key: '489915939841262', 
+cloudinary.config({
+    cloud_name: 'depjzfj9a',
+    api_key: '489915939841262',
     api_secret: '5tBdTUHJ33XMIN3iP-49Rfeps9I',
     // secure: true
 });
 
-class UserController{
-    static registerUser = async(req,res) => {
+class UserController {
+    static registerUser = async (req, res) => {
         // console.log(req.body)
         // console.log(req.files)
 
         const file = req.files.avatar
-        const myCloud = await cloudinary.uploader.upload(file.tempFilePath,{
-            folder : 'userImage'
+        const myCloud = await cloudinary.uploader.upload(file.tempFilePath, {
+            folder: 'userImage'
         })
 
-        const {name, userName, phone, email, password, conPassword, city, state, country} = req.body
-        const user = await UserModel.findOne({email:email})
+        const { name, userName, phone, email, password, conPassword, city, state, country } = req.body
+        const user = await UserModel.findOne({ email: email })
         if (user) {
             res.status(404).json({ status: "failed", message: "ᴛʜɪꜱ ᴇᴍᴀɪʟ ɪꜱ ᴀʟʀᴇᴀᴅʏ ᴇxɪᴛꜱ😓" });
         } else {
             if (name && email && password && conPassword) {
                 if (password === conPassword) {
-                    try{
+                    try {
                         // const salt = await bcrypt.genSalt(10)
                         // const hashPassword = await bcrypt.hash(password,salt)
-                        const hashPassword = await bcrypt.hash(password,10)
+                        const hashPassword = await bcrypt.hash(password, 10)
                         const data = new UserModel({
                             name: name,
                             userName: userName,
@@ -47,9 +47,9 @@ class UserController{
                         })
                         await data.save()
                         res
-                        .status(201)
-                        .json({ status: "success", message: "User Registration Successfully 😃🍻"});
-                    }catch(err){
+                            .status(201)
+                            .json({ status: "success", message: "User Registration Successfully 😃🍻" });
+                    } catch (err) {
                         console.log(err)
                     }
                 } else {
@@ -61,24 +61,24 @@ class UserController{
         }
     }
 
-    static loginUser = async(req,res) => {
-        try{
+    static loginUser = async (req, res) => {
+        try {
             // console.log(req.body)
-            const {email, password} = req.body
+            const { email, password } = req.body
             // console.log(password)
             if (email && password) {
-                const user = await UserModel.findOne({email : email})
+                const user = await UserModel.findOne({ email: email })
                 // console.log(user)
                 if (user != null) {
-                    const isMatched = await bcrypt.compare(password,user.password)
+                    const isMatched = await bcrypt.compare(password, user.password)
                     if ((user.email === email) && isMatched) {
                         //generate jwt token
                         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY);
                         // console.log(token)
-                        res.cookie('token',token)
+                        res.cookie('token', token)
                         res
-                        .status(201)
-                        .json({ status: "success", message: "Login successfully with web token 😃🍻", token, user});
+                            .status(201)
+                            .json({ status: "success", message: "Login successfully with web token 😃🍻", token, user });
                     } else {
                         res.status(401).json({ status: "failed", message: "'Email and Password is not valid !😓" });
                     }
@@ -88,12 +88,12 @@ class UserController{
             } else {
                 res.status(401).json({ status: "failed", message: "'All Fields are required 😓" });
             }
-        }catch(err){
+        } catch (err) {
             console.log(err)
         }
     }
 
-    static logout = async(req,res) => {
+    static logout = async (req, res) => {
         // try{
         //     res.clearCookie('token')
         //     res.send({ status: "success", message: "Logout successfully !😓" });
@@ -115,9 +115,9 @@ class UserController{
         }
     }
 
-    static updatePassword = async(req,res) => {
+    static updatePassword = async (req, res) => {
         // console.log(req.user)
-        try{
+        try {
             const { oldPassword, newPassword, confirmPassword } = req.body
 
             if (oldPassword && newPassword && confirmPassword) {
@@ -129,127 +129,130 @@ class UserController{
                 } else {
                     if (newPassword !== confirmPassword) {
                         res.status(201)
-                        .json({ "status": "failed", "message": "password does not match" })
+                            .json({ "status": "failed", "message": "password does not match" })
                     } else {
                         const salt = await bcrypt.genSalt(10)
                         const newHashPassword = await bcrypt.hash(newPassword, salt)
                         //console.log(req.user)
                         await UserModel.findByIdAndUpdate(req.user.id, { $set: { password: newHashPassword } })
                         res.status(201)
-                        .json({ "status": "success", "message": "Password changed succesfully" })
+                            .json({ "status": "success", "message": "Password changed succesfully" })
                     }
                 }
             } else {
                 res.status(201)
-                        .json({ "status": "failed", "message": "All Fields are Required" })
+                    .json({ "status": "failed", "message": "All Fields are Required" })
             }
-        }catch(err){
+        } catch (err) {
             res.status(201)
-                        .json(err)
+                .json(err)
         }
     }
 
-    static updateProfile = async(req,res) => {
-        try{
-            // console.log(req.params.id)
-            // console.log(req.body)
-            // console.log(req.files.avatar)
-            // const { name, email } = req.body
-            const { name, userName, phone, city, state, country } = req.body
-            // console.log(user)
-            const userImg = await UserModel.findById(req.params.id)
-            // console.log(userImg)
-            const imageId = userImg.avatar.public_id
-            // console.log(imageId)
-            await cloudinary.uploader.destroy(imageId)
-            const file = req.files.avatar
-            // console.log(file)
-            const myCloud = await cloudinary.uploader.upload(file.tempFilePath,{
-                folder : 'userImage'
-            })
-            const data = await UserModel.findByIdAndUpdate(req.params.id,{
-                name : name,
-                userName : userName, 
-                phone : phone, 
-                city : city, 
-                state : state, 
-                country : country,
-                avatar: {
-                    public_id: myCloud.public_id,
-                    url: myCloud.secure_url,
-                },
-            })
-            await data.save()
-            res
-            .status(201)
-            .json({ status: "success", message: "User Profile updated Successfully 😃🍻", data});
-        }catch(err){
-            res.json(err)
+
+    static updateProfile = async (req, res) => {
+        try {
+            //console.log(req.body)
+            if (req.file) {
+                const user = await userModel.findById(req.user.id);
+                const image_id = user.avatar.public_id;
+                await cloudinary.uploader.destroy(image_id);
+
+                const file = req.files.avatar;
+                const myimage = await cloudinary.uploader.upload(file.tempFilePath, {
+                    folder: "userImage",
+                    width: 150,
+                });
+                var data = {
+                    name: req.body.name,
+                    email: req.body.email,
+                    avatar: {
+                        public_id: myimage.public_id,
+                        url: myimage.secure_url,
+                    },
+                };
+            } else {
+                var data = {
+                    name: req.body.name,
+                    email: req.body.email,
+                };
+            }
+
+            const updateuserprofile = await userModel.findByIdAndUpdate(
+                req.user.id,
+                data
+            );
+            res.status(200).json({
+                success: true,
+                updateuserprofile,
+            });
+        } catch (error) {
+            console.log(error);
         }
-    }
+    };
 
     //AdminOnly
-    static getAllUser = async(req,res) => {
-        try{
+    static getAllUser = async (req, res) => {
+        try {
             const data = await UserModel.find()
             res.status(200).json({
                 data
             })
-        }catch(err){
+        } catch (err) {
             res.send(err)
         }
     }
 
-    static getSingleUser = async(req,res) => {
-        try{
+    static getSingleUser = async (req, res) => {
+        try {
             const data = await UserModel.findById(req.params.id)
             res.status(200).json({
                 success: true,
                 data
             })
-        }catch(err){
+        } catch (err) {
             console.log(err)
         }
     }
 
     static getUserDetail = async (req, res) => {
         try {
-        //   console.log(req.user);
-          const user = await UserModel.findById(req.user.id);
-    
-          res.status(200).json({
-            success: true,
-            user,
-          });
+            //   console.log(req.user);
+            const user = await UserModel.findById(req.user.id);
+
+            res.status(200).json({
+                success: true,
+                user,
+            });
         } catch (error) {
-          console.log(error);
+            console.log(error);
         }
     };
 
-    static deleteUser = async(req,res) => {
-        try{
+    static deleteUser = async (req, res) => {
+        try {
             const data = await UserModel.findByIdAndDelete(req.params.id)
             res
-            .status(200)
-            .json({ status: "success", message: "User deleted successfully 😃🍻"});
-        }catch(err){
+                .status(200)
+                .json({ status: "success", message: "User deleted successfully 😃🍻" });
+        } catch (err) {
             console.log(err)
         }
     }
 
-    static updateUserRole = async(req,res) => {
-        try{
+    static updateUserRole = async (req, res) => {
+        try {
             // console.log(req.body)
             // const { role } = req.body
             const { name, email, password, avatar, role } = req.body
-            const data = await UserModel.findByIdAndUpdate(req.params.id,{
-                role : role,
+            const data = await UserModel.findByIdAndUpdate(req.params.id, {
+                role: role,
             })
             await data.save()
             res
-            .status(201)
-            .json({ status: "success", message: "User Role updated Successfully 😃🍻"});
-        }catch(err){
+                .status(201)
+                .json({ status: "success", message: "User Role updated Successfully 😃🍻" });
+        } catch (err) {
             res.send(err)
         }
     }
--- routes/api.js
+++ routes/api.js
@@ -10,44 +10,44 @@ const router = express.Router()
 
 
 //UserController API Route
-router.post('/register',UserController.registerUser)
-router.post('/userLogin',UserController.loginUser)
-router.post('/updatePassword/:id',ChangeUserAuth,UserController.updatePassword)
-router.get('/logout',UserController.logout)
-router.get('/me',ChangeUserAuth,UserController.getUserDetail)
+router.post('/register', UserController.registerUser)
+router.post('/userLogin', UserController.loginUser)
+router.post('/updatePassword/:id', ChangeUserAuth, UserController.updatePassword)
+router.get('/logout', UserController.logout)
+router.get('/me', ChangeUserAuth, UserController.getUserDetail)
 
 //AdminOnly
-router.get('/admin/users',UserController.getAllUser)
-router.get('/admin/getUser/:id',UserController.getSingleUser)
-router.delete('/admin/deleteUser/:id',UserController.deleteUser)
-router.put('/admin/updateUserRole/:id',UserController.updateUserRole)
-router.post('/admin/updateProfile/:id',UserController.updateProfile)
+router.get('/admin/users', UserController.getAllUser)
+router.get('/admin/getUser/:id', UserController.getSingleUser)
+router.delete('/admin/deleteUser/:id', UserController.deleteUser)
+router.put('/admin/updateUserRole/:id', UserController.updateUserRole)
+router.post('/updateProfile', ChangeUserAuth, UserController.updateProfile)
 
 //ProductController
-router.post('/product/create',ProductController.createProduct)
-router.get('/products',ProductController.getAllProducts)
-router.get('/getProductDetail/:id',ProductController.getProductDetail)
-router.get('/product/getAdminProduct',ProductController.getAdminProduct)
-router.post('/product/updateProduct',ProductController.updateProduct)
-router.get('/product/deleteProduct/:id',ProductController.deleteProduct)
+router.post('/product/create', ProductController.createProduct)
+router.get('/products', ProductController.getAllProducts)
+router.get('/getProductDetail/:id', ProductController.getProductDetail)
+router.get('/product/getAdminProduct', ProductController.getAdminProduct)
+router.post('/product/updateProduct', ProductController.updateProduct)
+router.get('/product/deleteProduct/:id', ProductController.deleteProduct)
 
 //OrderController
-router.post('/order/create',OrderController.newOrder)
-router.get('/order/getSingleOrder/:id',OrderController.getSingleOrder)
-router.get('/order/myOrder',OrderController.myOrder)
-router.get('/order/getAllOrders',OrderController.getAllOrders)
-router.get('/order/deleteOrder/:id',OrderController.deleteOrder)
+router.post('/order/create', OrderController.newOrder)
+router.get('/order/getSingleOrder/:id', OrderController.getSingleOrder)
+router.get('/order/myOrder', OrderController.myOrder)
+router.get('/order/getAllOrders', OrderController.getAllOrders)
+router.get('/order/deleteOrder/:id', OrderController.deleteOrder)
 
 
 //CategoryController
-router.post('/category/create',CategoryController.createCategory)
-router.get('/categories',CategoryController.getAllCategories)
-router.get('/getCategoriesDetail/:id',CategoryController.getCategoryDetail)
+router.post('/category/create', CategoryController.createCategory)
+router.get('/categories', CategoryController.getAllCategories)
+router.get('/getCategoriesDetail/:id', CategoryController.getCategoryDetail)
 
 
 //payment
-router.post('/payment/process',ChangeUserAuth,PaymentController.processPayment)
-router.get('/stripeapiKey',ChangeUserAuth,PaymentController.sendStripeApiKey)
+router.post('/payment/process', ChangeUserAuth, PaymentController.processPayment)
+router.get('/stripeapiKey', ChangeUserAuth, PaymentController.sendStripeApiKey)
 
 
 
